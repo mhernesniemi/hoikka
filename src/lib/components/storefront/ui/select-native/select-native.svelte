8e3a721885843d51d180e4d2c4911f0c843f597a
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2026-02-23 13:04:41 +0200

    style select component

--- src/lib/components/storefront/ui/select-native/select-native.svelte
+++ src/lib/components/storefront/ui/select-native/select-native.svelte
@@ -13,7 +13,7 @@
 
 <select
   class={cn(
-    "rounded-md border border-gray-300 bg-white px-3 py-1.5 text-sm text-gray-700 hover:border-gray-400 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none",
+    "rounded-md border border-gray-300 bg-white px-3 py-1.5 pr-8 text-sm text-gray-700 hover:border-gray-400",
     className
   )}
   bind:value
