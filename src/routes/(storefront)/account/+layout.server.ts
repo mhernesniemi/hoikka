88b8a7ae8b66e4b2ec953371226e7802d16354eb
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2026-02-23 02:35:40 +0200

    create admin customer if not existing

--- src/routes/(storefront)/account/+layout.server.ts
+++ src/routes/(storefront)/account/+layout.server.ts
@@ -3,6 +3,7 @@
  * Requires user to be logged in
  */
 import { redirect } from "@sveltejs/kit";
+import { customerService } from "$lib/server/services/customers.js";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
@@ -17,7 +18,19 @@ export const load: LayoutServerLoad = async ({ locals }) => {
 		);
 	}
 
+	// Create customer record if it doesn't exist (e.g. admin users)
+	let customer = locals.customer;
+	if (!customer) {
+		customer = await customerService.create({
+			authUserId: locals.user.id,
+			email: locals.user.email,
+			firstName: locals.user.name?.split(" ")[0] ?? "",
+			lastName: locals.user.name?.split(" ").slice(1).join(" ") ?? ""
+		});
+		locals.customer = customer;
+	}
+
 	return {
-		customer: locals.customer
+		customer
 	};
 };
